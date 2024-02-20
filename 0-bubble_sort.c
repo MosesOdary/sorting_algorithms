23dee02614ba55e4c4f435aab120248ea1a37672
= <mosesodary101@gmail.com>
2024-02-21 01:37:06 +0300

    Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,5 +4,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
+	uint16_t i = 0;
+	uint16_t j = 0;
+	uint64_t temp = 0;
 
+	for (; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				print_array(array, size);
+			}
+		}
+	}
 }
+
