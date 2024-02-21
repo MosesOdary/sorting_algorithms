3ef1ecbd6b3d3e91c43aee3b8ecd3cbea0f49f55
= <=>
2024-02-21 05:01:25 +0300

    Advanced Tasks

---
+++ 100-shell_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap_ints - ints from array being swapped
+ * @a: int 1 being swapped
+ * @b: int 2 being swapped
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - Sorts integers from array; uses asending order
+ * @array: memory block of integers
+ * @size: size of memory block(array)
+ *
+ * Description: Knuth sequence of interval is provided.
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
