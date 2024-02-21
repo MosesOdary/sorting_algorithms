f1512a8ff4e7b21180b86a24670726f6ab8ff298
= <=>
2024-02-21 03:37:40 +0300

    Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,17 @@
 #include "sort.h"
 
-
 void insertion_sort_list(listint_t **list)
 {
-	
+	for (size_t i = 1; i < length; i++)
+    {
+        size_t moveItem = i;
+        int insert = array[i];
+
+        while (moveItem > 0 && array[moveItem - 1] > insert)
+        {
+            array[moveItem] = array[moveItem - 1];
+            --moveItem;
+        }
+        array[moveItem] = insert;
+    }
 }
